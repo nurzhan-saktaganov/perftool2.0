941539b01fbb299c991681c7c2704d0076f5e70a
Nurzhan Saktaganov <nur92world@mail.ru>
2017-03-12 21:10:51 +0300

    Context and etc. types

---
+++ context_types.c
@@ -0,0 +1,196 @@
+#include <stdio.h>
+#include <string.h>
+#include "context_string.h"
+#include "context_types.h"
+
+context_type get_context_type_from_name(const char *context_name)
+{
+	if (context_name == NULL){
+		return UNKNOWN_CONTEXT;
+	} else if (strcmp(context_name, "arr_name") == 0){
+		return ARRAY_NAME;
+	} else if (strcmp(context_name, "barrier") == 0){
+		return BARRIER;
+	} else if (strcmp(context_name, "critical") == 0){
+		return CRITICAL;
+	} else if (strcmp(context_name, "filename") == 0){
+		return FILENAME;
+	} else if (strcmp(context_name, "flush") == 0){
+		return FLUSH;
+	} else if (strcmp(context_name, "function") == 0){
+		return FUNCTION;
+	} else if (strcmp(context_name, "func_call") == 0){
+		return FUNC_CALL;
+	} else if (strcmp(context_name, "master") == 0){
+		return MASTER;
+	} else if (strcmp(context_name, "omploop") == 0){
+		return OMPLOOP;
+	} else if (strcmp(context_name, "ordered") == 0){
+		return ORDERED;
+	} else if (strcmp(context_name, "parallel") == 0){
+		return PARALLEL;
+	} else if (strcmp(context_name, "sections") == 0){
+		return SECTIONS;
+	} else if (strcmp(context_name, "sect_ev") == 0){
+		return SECTION_EVENT;
+	} else if (strcmp(context_name, "seqloop") == 0){
+		return SEQLOOP;
+	} else if (strcmp(context_name, "single") == 0){
+		return SINGLE;
+	} else if (strcmp(context_name, "threadprivate") == 0){
+		return THREADPRIVATE;
+	} else if (strcmp(context_name, "var_name") == 0){
+		return VARIABLE_NAME;
+	} else if (strcmp(context_name, "workshare") == 0){
+		return WORKSHARE;
+	}
+	fprintf (stderr, "Unknown context name: %s\n", context_name); 
+	return UNKNOWN_CONTEXT;
+}
+
+redop_type get_redop_type_from_name(const char *redop_name)
+{
+	if (redop_name == NULL){
+		return UNKNOWN_REDOP;
+	} else if (strcmp(redop_name, "PLUS") == 0){
+		return PLUS;
+	} else if (strcmp(redop_name, "MINUS") == 0){
+		return MINUS;
+	} else if (strcmp(redop_name, "PRODUCT") == 0){
+		return PRODUCT;
+	} else if (strcmp(redop_name, "AND") == 0){
+		return AND;
+	} else if (strcmp(redop_name, "OR") == 0){
+		return OR;
+	} else if (strcmp(redop_name, "EQV") == 0){
+		return EQV;
+	} else if (strcmp(redop_name, "NEQV") == 0){
+		return NEQV;
+	} else if (strcmp(redop_name, "MAX") == 0){
+		return MAX;
+	} else if (strcmp(redop_name, "MIN") == 0){
+		return MIN;
+	} else if (strcmp(redop_name, "IAND") == 0){
+		return IAND;
+	} else if (strcmp(redop_name, "IOR") == 0){
+		return IOR;
+	} else if (strcmp(redop_name, "IEOR") == 0){
+		return IEOR;
+	}
+	fprintf(stderr, "Unknown redop name: %s\n", redop_name);
+	return UNKNOWN_REDOP;
+}
+
+behavior_type get_behavior_type_from_name(const char *behavior_name)
+{
+	if (behavior_name == NULL){
+		return UNKNOWN_BEHAVIOR;
+	} else if (strcmp(behavior_name, "PRIVATE") == 0){
+		return PRIVATE;
+	} else if (strcmp(behavior_name, "SHARED") == 0){
+		return SHARED;
+	} else if (strcmp(behavior_name, "NONE") == 0){
+		return NONE;
+	}
+	fprintf(stderr, "Unknown behavior name: %s\n", behavior_name);
+	return UNKNOWN_BEHAVIOR;
+}
+
+schedule_type get_schedule_type_from_name(const char *schedule_name)
+{
+	if (schedule_name == NULL){
+		return UNKNOWN_SCHEDULING;
+	} else if (strcmp(schedule_name, "STATIC") == 0){
+		return STATIC;
+	} else if (strcmp(schedule_name, "DYNAMIC") == 0){
+		return DYNAMIC;
+	} else if (strcmp(schedule_name, "RUNTIME") == 0){
+		return RUNTIME;
+	}
+	fprintf(stderr, "Unknown schedule name: %s\n", schedule_name);
+	return UNKNOWN_SCHEDULING;
+}
+
+variable_rt_type get_variable_rt_type_from_name(const char *rt_type_name)
+{
+	if (rt_type_name == NULL) {
+		return UNKNOWN_RT_TYPE;
+	} else if (strcmp(rt_type_name, "0") == 0) {
+		return RT_CHAR;
+	} else if (strcmp(rt_type_name, "1") == 0) {
+		return RT_INT;
+	} else if (strcmp(rt_type_name, "2") == 0) {
+		return RT_LONG;
+	} else if (strcmp(rt_type_name, "3") == 0) {
+		return RT_FLOAT;
+	} else if (strcmp(rt_type_name, "4") == 0) {
+		return RT_DOUBLE;
+	} else if (strcmp(rt_type_name, "5") == 0) {
+		return RT_FLOAT_COMPLEX;
+	} else if (strcmp(rt_type_name, "6") == 0) {
+		return RT_DOUBLE_COMPLEX;
+	}
+	fprintf(stderr, "Unknown rt type name: %s\n", rt_type_name);
+	return UNKNOWN_RT_TYPE;
+}
+
+context_type get_context_string_type(const char *context_string)
+{
+	context_type type;
+	char *context_type_name = get_param_str_value(context_string, "type");
+	type = get_context_type_from_name(context_type_name);
+	if (context_type_name != NULL){
+		free(context_type_name);
+	}
+	return type;
+}
+
+/*
+const char * get_context_type_name(context_type type)
+{
+	switch (type){
+		case ARRAY_NAME:
+			return "arr_name";
+		case BARRIER:
+			return "barrier";
+		case COMMON_NAME:
+			return "common_name";
+		case CRITICAL:
+			return "critical";
+		case FILENAME:
+			return "filename";
+		case FLUSH:
+			return "flush";
+		case FUNCTION:
+			return "function";
+		case FUNC_CALL:
+			return "func_call";
+		case MASTER:
+			return "master";
+		case OMPLOOP:
+			return "omploop";
+		case ORDERED:
+			return "ordered";
+		case PARALLEL:
+			return "parallel";
+		case SECTIONS:
+			return "sections";
+		case SECTION_EVENT:
+			return "sect_ev";
+		case SEQLOOP:
+			return "seqloop";
+		case SINGLE:
+			return "single";
+		case THREADPRIVATE:
+			return "threadprivate";
+		case UNKNOWN_CONTEXT:
+			return "unknown";
+		case VARIABLE_NAME:
+			return "var_name";
+		case WORKSHARE:
+			return "workshare";
+		default:
+			return "unknown";
+	}	
+}
+*/
---
+++ context_types.h
@@ -0,0 +1,240 @@
+#ifndef CONTEXT_TYPES_H
+#define CONTEXT_TYPES_H
+
+typedef enum {
+	UNKNOWN,
+	ARRAY_NAME,
+	BARRIER,
+	COMMON_NAME,
+	CRITICAL,
+	FILENAME,
+	FLUSH,
+	FUNCTION,
+	FUNC_CALL,
+	MASTER,
+	OMPLOOP,
+	ORDERED,
+	PARALLEL,
+	SECTIONS,
+	SECTION_EVENT,
+	SEQLOOP,
+	SINGLE,
+	THREADPRIVATE,
+	VARIABLE_NAME,
+	WORKSHARE,
+} context_type;
+
+typedef enum {
+	UNKNOWN_REDOP,
+	AND,
+	EQV,
+	IAND,
+	IEOR,
+	IOR,
+	MAX,
+	MIN,
+	MINUS,
+	NEQV,
+	OR,
+	PLUS,
+	PRODUCT,
+} redop_type;
+
+typedef enum {
+	UNKNOWN_BEHAVIOR,
+	NONE,
+	PRIVATE,
+	SHARED,
+} behavior_type;
+
+typedef enum {
+	UNKNOWN_SCHEDULING,
+	DYNAMIC,
+	GUIDED,
+	RUNTIME,
+	STATIC,
+} schedule_type;
+
+typedef enum {
+	UNKNOWN_RT_TYPE,
+	RT_CHAR,
+	RT_DOUBLE,
+	RT_DOUBLE_COMPLEX,
+	RT_FLOAT,
+	RT_FLOAT_COMPLEX,
+	RT_INT,
+	RT_LONG,
+} variable_rt_type;
+
+typedef struct {
+	int count; // количество имен
+	char **names; //список имен
+} names_list;
+
+typedef struct {
+	char *filename;
+	int begin_line;
+	int end_line;
+	names_list *private;
+	names_list *shared;
+	names_list *firstprivate;
+	names_list *copyin;
+	names_list *reduction;
+	redop_type redop;
+	behavior_type default_behavior;
+	char *if_text;
+	char *num_threads;
+} parallel_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int begin_line;
+	int end_line;
+	int is_ordered;
+	int is_nowait;
+	names_list *private;
+	names_list *firstprivate;
+	names_list *lastprivate;
+	names_list *reduction;
+	redop_type redop;
+	schedule_type schedule;
+	char *chunk_size;
+} omploop_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int begin_line;
+	int end_line;
+	int is_nowait;
+	names_list *private;
+	names_list *firstprivate;
+	names_list *lastprivate;
+	names_list *reduction;
+	redop_type redop;
+} sections_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int begin_line;
+	int end_line;
+} section_event_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int begin_line;
+	int end_line;
+	int is_nowait;
+	names_list *private;
+	names_list *firstprivate;
+	names_list *copyprivate;
+} single_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int begin_line;
+	int end_line;
+	int is_nowait;
+} workshare_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int begin_line;
+	int end_line;
+} master_context_descriptor;
+
+typedef struct {
+	char *filename;
+	char *name;
+	int begin_line;
+	int end_line;
+} critical_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int line_number;
+} barrier_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int line_number;
+	names_list *flushed;
+} flush_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int begin_line;
+	int end_line;
+} ordered_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int line_number;
+	names_list *threadprivate;
+} threadprivate_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int line_number;
+	char *var_name;
+	variable_rt_type variable_type;
+	int is_indata;
+	int is_incommon;
+	int is_insave;
+} variable_name_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int line_number;
+	char *arr_name;
+	variable_rt_type variable_type;
+	int rank;
+	int is_indata;
+	int is_incommon;
+	int is_insave;
+} array_name_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int line_number;
+	char *block_name;
+	names_list *components;
+} common_name_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int line_number;
+} file_name_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int begin_line;
+	int end_line;
+} seqloop_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int line_number;
+	char *name;
+	int args_count;
+} func_call_context_descriptor;
+
+typedef struct {
+	char *filename;
+	int begin_line;
+	int end_line;
+	char *func_name;
+	int args_count;
+} function_context_descriptor;
+
+typedef struct {
+	context_type type;
+	void *context_ptr;
+} context_descriptor;
+
+context_type get_context_string_type(const char *context_string);
+redop_type get_redop_type_from_name(const char *redop_name);
+behavior_type get_behavior_type_from_name(const char *behavior_name);
+schedule_type get_schedule_type_from_name(const char *schedule_name);
+variable_rt_type get_variable_rt_type_from_name(const char *rt_type_name);
+
+#endif
