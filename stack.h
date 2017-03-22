484381b4a12de9586e532dee7627f9661dfd73dd
Nurzhan Saktaganov <nur92world@mail.ru>
2017-03-22 04:05:07 +0300

    typo fix: list->stack

--- stack.h
+++ stack.h
@@ -6,7 +6,7 @@
 typedef list stack;
 
 stack *stack_create();
-void stack_destroy(list *s_p, void (*destroy_data)(void *));
+void stack_destroy(stack *s_p, void (*destroy_data)(void *));
 void stack_push(stack *s_p, void *data);
 void *stack_pop(stack *s_p);
 void *stack_peek(stack *s_p);
