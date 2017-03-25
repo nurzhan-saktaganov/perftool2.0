feb6032506d925a7279911fc468cd650e44ac036
Nurzhan Saktaganov <nur92world@mail.ru>
2017-03-25 20:48:15 +0300

    create lib with dvmh_omp_event.c

--- create_lib.sh
+++ create_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rm list.o context_string.o context_types.o register_context.o omp_dbg.o
-icc -c list.c context_string.c context_types.c register_context.c omp_dbg.c &&\
-ar cr omp_dbg.lib list.o context_string.o context_types.o register_context.o omp_dbg.o
+rm list.o context_string.o context_types.o register_context.o omp_dbg.o dvmh_omp_event.o
+icc -c list.c context_string.c context_types.c register_context.c omp_dbg.c dvmh_omp_event.c -openmp &&\
+ar cr omp_dbg.lib list.o context_string.o context_types.o register_context.o omp_dbg.o dvmh_omp_event.o
