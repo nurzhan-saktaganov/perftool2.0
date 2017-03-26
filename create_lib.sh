f2cefba45cbb96a36facae0b003e252f0ea50af2
Nurzhan Saktaganov <nur92world@mail.ru>
2017-03-26 05:34:21 +0300

    add dvmh_omp_thread_info to create_lib.sh

--- create_lib.sh
+++ create_lib.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-rm list.o context_string.o context_types.o register_context.o omp_dbg.o dvmh_omp_event.o
-icc -c list.c context_string.c context_types.c register_context.c omp_dbg.c dvmh_omp_event.c -openmp &&\
-ar cr omp_dbg.lib list.o context_string.o context_types.o register_context.o omp_dbg.o dvmh_omp_event.o
+rm list.o stack.o context_string.o context_types.o\
+	register_context.o omp_dbg.o dvmh_omp_event.o\
+	dvmh_omp_thread_info.o
+
+icc -c list.c stack.c context_string.c context_types.c\
+	register_context.c omp_dbg.c dvmh_omp_event.c\
+	dvmh_omp_thread_info.c -openmp &&\
+\
+ar cr omp_dbg.lib list.o stack.o context_string.o context_types.o\
+	 register_context.o omp_dbg.o dvmh_omp_event.o\
+	dvmh_omp_thread_info.o
+
