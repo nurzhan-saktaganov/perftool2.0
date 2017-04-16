c5d3e867e8a1aa60189e16304dc1f5bfdb33ac3e
Nurzhan Saktaganov <nur92world@mail.ru>
2017-04-17 02:22:15 +0300

    building intervals

--- dvmh_omp_interval.c
+++ dvmh_omp_interval.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "stack.h"
 #include "list.h"
 #include "context_types.h"
 #include "dvmh_omp_event.h"
@@ -10,10 +11,17 @@
 #define HASH_FIND_LONG(head,findlong,out)                                          \
     HASH_FIND(hh,head,findlong,sizeof(long),out)
 
-dvmh_omp_interval *dvmh_omp_interval_create();
-void dvmh_omp_interval_set_descriptor(dvmh_omp_interval *i, context_descriptor *d);
+typedef struct _events_occurrences {
+    long thread_id;     /* this is key */
+    list *events; /* this is value */
+    UT_hash_handle hh;  /* makes this structure hashable */
+} events_occurrences;
 
-dvmh_omp_interval *current_interval = NULL;
+typedef struct _dvmh_omp_interval {
+    context_descriptor *descriptor;
+    list *subintervals;
+    events_occurrences *occurrences; //hashtable := thread_id to list of events
+} dvmh_omp_interval;
 
 typedef struct _registered_interval {
     context_descriptor *descriptor;  /* this is key*/
@@ -21,29 +29,98 @@ typedef struct _registered_interval {
     UT_hash_handle hh;
 } registered_interval;
 
-registered_interval *registered_intervals = NULL; /* this is hash */
+typedef struct _building_context {
+    stack *active_intervals;
+    registered_interval *registered_intervals; /* this is hash, must be NULL initialized */
+} building_context;
 
-dvmh_omp_interval *dvmh_omp_interval_register(context_descriptor *d)
+
+/* list of functions for building context */
+
+
+/* list of functions for interval */
+dvmh_omp_interval *dvmh_omp_interval_create();
+void dvmh_omp_interval_add_occurrence(dvmh_omp_interval *i, dvmh_omp_event *e);
+void dvmh_omp_interval_add_subinterval(dvmh_omp_interval *i, dvmh_omp_interval *s);
+
+building_context *building_context_create()
+{
+    building_context *bc = (building_context *) malloc(sizeof(building_context));
+    bc->active_intervals = stack_create();
+    bc->registered_intervals = NULL;
+    return bc;
+}
+
+void building_context_destroy(building_context *bc)
+{
+    //TODO destroy bc->registered_intervals
+    stack_destroy(bc->active_intervals, NULL);
+    free(bc);
+}
+
+void building_context_set_active_interval(building_context *bc, dvmh_omp_interval *i)
+{
+    stack_push(bc->active_intervals, i);
+}
+
+void building_context_finish_active_interval(building_context *bc)
+{
+    stack_pop(bc->active_intervals);
+}
+
+dvmh_omp_interval *building_context_get_active_interval(building_context *bc)
+{
+    return (dvmh_omp_interval *) stack_peek(bc->active_intervals);
+}
+
+dvmh_omp_interval *building_context_register_interval(building_context *bc, context_descriptor *d)
 {
     registered_interval *r;
-    HASH_FIND_PTR(registered_intervals, &d, r);
+    HASH_FIND_PTR(bc->registered_intervals, &d, r);
     if (r == NULL){
         r = (registered_interval *) malloc(sizeof(registered_interval));
-        r->interval = dvmh_omp_interval_create();
-        dvmh_omp_interval_set_descriptor(r->interval, d);
-        HASH_ADD_PTR(registered_intervals, descriptor, r);
+        r->interval = dvmh_omp_interval_create(d);
+        HASH_ADD_PTR(bc->registered_intervals, descriptor, r);
     }
     return r->interval;
 }
 
-void dvmh_omp_interval_finalize()
+dvmh_omp_interval *build_intervals(building_context *bc, dvmh_omp_event *e)
 {
-    //TODO delete registered_intervals
+    dvmh_omp_interval *i = NULL;
+    dvmh_omp_event_type t = dvmh_omp_event_get_type(e);
+    if (t == DVMH_OMP_EVENT_INIT){
+        i = building_context_register_interval(bc, NULL);
+        dvmh_omp_interval_add_occurrence(i, e);
+        building_context_set_active_interval(bc, i);
+    }
+    if (t == DVMH_OMP_EVENT_BEFORE_INTERVAL){
+        context_descriptor *d = dvmh_omp_event_get_context_descriptor(e);
+        i = building_context_register_interval(bc, d);
+        dvmh_omp_interval *parent = building_context_get_active_interval(bc);
+        dvmh_omp_interval_add_subinterval(parent, i);
+        dvmh_omp_interval_add_occurrence(i, e);
+        building_context_set_active_interval(bc, i);
+    }
+
+    dvmh_omp_subevent_iterator *it = dvmh_omp_subevent_iterator_new(e);
+	while (dvmh_omp_subevent_iterator_has_next(it)){
+		dvmh_omp_event *subevent = dvmh_omp_subevent_iterator_next(it);
+		build_intervals(bc, subevent);
+	}
+	dvmh_omp_subevent_iterator_destroy(it);
+
+    if (t == DVMH_OMP_EVENT_INIT || t == DVMH_OMP_EVENT_BEFORE_INTERVAL){
+        building_context_finish_active_interval(bc);
+    }
+
+    return i;
 }
 
-dvmh_omp_interval *dvmh_omp_interval_create()
+dvmh_omp_interval *dvmh_omp_interval_create(context_descriptor *d)
 {
     dvmh_omp_interval *i = (dvmh_omp_interval *) malloc(sizeof(dvmh_omp_interval));
+    i->descriptor = d;
     i->subintervals = list_create();
     i->occurrences = NULL;
     return i;
@@ -57,7 +134,7 @@ void dvmh_omp_interval_destroy(dvmh_omp_interval *i)
         dvmh_omp_interval_destroy(s);
     }
     list_destroy(i->subintervals, NULL);
-    // TODO destroy occurences;
+    // TODO destroy occurences hahtable
     free(i);
 }
 
@@ -66,11 +143,6 @@ void dvmh_omp_interval_add_subinterval(dvmh_omp_interval *i, dvmh_omp_interval *
     if (!list_has_element(i->subintervals, s)){
         list_append_tail(i->subintervals, s);
     }
-}
-
-void dvmh_omp_interval_set_descriptor(dvmh_omp_interval *i, context_descriptor *d)
-{
-    i->descriptor = d;
 }
 
 void dvmh_omp_interval_add_occurrence(dvmh_omp_interval *i, dvmh_omp_event *e)
@@ -85,4 +157,12 @@ void dvmh_omp_interval_add_occurrence(dvmh_omp_interval *i, dvmh_omp_event *e)
         HASH_ADD_LONG(i->occurrences, thread_id, o);
     }
     list_append_tail(o->events, e);
+}
+
+dvmh_omp_interval *dvmh_omp_interval_build(dvmh_omp_event *e)
+{
+    building_context *bc = building_context_create();
+    dvmh_omp_interval *i = build_intervals(bc, e);
+    building_context_destroy(bc);
+    return i;
 }
--- dvmh_omp_interval.h
+++ dvmh_omp_interval.h
@@ -1,35 +1,12 @@
-#ifndef INTERVAL_H
-#define INTERVAL_H
+#ifndef DVMH_OMP_INTERVAL_H
+#define DVMH_OMP_INTERVAL_H
 
-#include "list.h"
-#include "context_types.h"
-#include "uthash.h"
 #include "dvmh_omp_event.h"
-
-/*
-struct _events_occurrences;
-struct _dvmh_omp_interval;
 
 typedef struct _events_occurrences events_occurrences;
 typedef struct _dvmh_omp_interval dvmh_omp_interval;
-*/
 
-typedef struct _events_occurrences {
-    long thread_id;     /* key */
-    list *events;
-    UT_hash_handle hh;  /* makes this structure hashable */
-} events_occurrences;
-
-typedef struct _dvmh_omp_interval {
-    context_descriptor *descriptor;
-    list *subintervals;
-    events_occurrences *occurrences; //thread_id to list of events
-} dvmh_omp_interval;
-
-dvmh_omp_interval *dvmh_omp_interval_register(context_descriptor *d);
-void dvmh_omp_interval_finalize();
+dvmh_omp_interval *dvmh_omp_interval_build(dvmh_omp_event *e);
 void dvmh_omp_interval_destroy(dvmh_omp_interval *i);
-void dvmh_omp_interval_add_subinterval(dvmh_omp_interval *i, dvmh_omp_interval *s);
-void dvmh_omp_interval_add_occurrence(dvmh_omp_interval *i, dvmh_omp_event *e);
 
 #endif
