0f50ead8a02526ee372afda6e381264109ed23de
Nurzhan Saktaganov <nur92world@mail.ru>
2017-05-12 20:25:30 +0300

    remove object files after creating lib

--- create_lib.sh
+++ create_lib.sh
@@ -1,9 +1,4 @@
-#!/bin/sh
-rm list.o stack.o context_string.o context_descriptor.o\
-	register_context.o omp_dbg.o dvmh_omp_event.o\
-	dvmh_omp_thread_info.o dvmh_omp_event_analyzer.o\
-	dvmh_omp_interval.o
-
+#!/usr/bin/env bash
 icc -Wall -fPIC -c list.c stack.c context_string.c context_descriptor.c\
 	register_context.c omp_dbg.c dvmh_omp_event.c\
 	dvmh_omp_thread_info.c dvmh_omp_event_analyzer.c\
@@ -13,4 +8,9 @@ ar cr omp_dbg.lib list.o stack.o context_string.o context_descriptor.o\
 	register_context.o omp_dbg.o dvmh_omp_event.o\
 	dvmh_omp_thread_info.o dvmh_omp_event_analyzer.o\
 	dvmh_omp_interval.o
+
+ rm list.o stack.o context_string.o context_descriptor.o\
+    register_context.o omp_dbg.o dvmh_omp_event.o\
+    dvmh_omp_thread_info.o dvmh_omp_event_analyzer.o\
+    dvmh_omp_interval.o
 
--- mic_create_lib.sh
+++ mic_create_lib.sh
@@ -1,17 +1,16 @@
-#!/bin/sh
-rm list.o stack.o context_string.o context_descriptor.o\
+#!/usr/bin/env bash
+icc -Wall -fPIC -c list.c stack.c context_string.c context_descriptor.c\
+	register_context.c omp_dbg.c dvmh_omp_event.c\
+	dvmh_omp_thread_info.c dvmh_omp_event_analyzer.c\
+	dvmh_omp_interval.c -mmic -qopenmp &&\
+\
+ar cr omp_dbg.lib list.o stack.o context_string.o context_descriptor.o\
 	register_context.o omp_dbg.o dvmh_omp_event.o\
 	dvmh_omp_thread_info.o dvmh_omp_event_analyzer.o\
 	dvmh_omp_interval.o
 
-icc -Wall -c list.c stack.c context_string.c context_descriptor.c\
-	register_context.c omp_dbg.c dvmh_omp_event.c\
-	dvmh_omp_thread_info.c dvmh_omp_event_analyzer.c\
-	dvmh_omp_interval.c -mmic	 -qopenmp &&\
-\
-/opt/mpss/3.6.1/sysroots/x86_64-mpsssdk-linux/usr/libexec/k1om-mpss-linux/gcc/k1om-mpss-linux/5.1.1/ar\
- cr omp_dbg.lib list.o stack.o context_string.o context_descriptor.o\
-	register_context.o omp_dbg.o dvmh_omp_event.o\
-	dvmh_omp_thread_info.o dvmh_omp_event_analyzer.o\
-	dvmh_omp_interval.o
+ rm list.o stack.o context_string.o context_descriptor.o\
+    register_context.o omp_dbg.o dvmh_omp_event.o\
+    dvmh_omp_thread_info.o dvmh_omp_event_analyzer.o\
+    dvmh_omp_interval.o
 
