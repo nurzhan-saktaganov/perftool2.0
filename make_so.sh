07a2eb02c73ae11f2ec951b060b9e5d5f5b98e9b
Nurzhan Saktaganov <nur92world@mail.ru>
2017-05-12 16:14:29 +0300

    creating .so file using exportmap

---
+++ make_so.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+icc -Wall -shared -qopenmp -fPIC\
+	-Wl,--version-script=exportmap.exp\
+	-o omp_dbg.lib\
+	list.c stack.c context_string.c context_descriptor.c\
+	register_context.c omp_dbg.c dvmh_omp_event.c\
+	dvmh_omp_thread_info.c dvmh_omp_event_analyzer.c\
+	dvmh_omp_interval.c
