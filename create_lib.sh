2c7cdf2677093c4e539d50566aca647183c153c2
Nurzhan Saktaganov <nur92world@mail.ru>
2017-05-12 16:14:53 +0300

    -fPIC flag for create_lib.sh

--- create_lib.sh
+++ create_lib.sh
@@ -4,7 +4,7 @@ rm list.o stack.o context_string.o context_descriptor.o\
 	dvmh_omp_thread_info.o dvmh_omp_event_analyzer.o\
 	dvmh_omp_interval.o
 
-icc -Wall -c list.c stack.c context_string.c context_descriptor.c\
+icc -Wall -fPIC -c list.c stack.c context_string.c context_descriptor.c\
 	register_context.c omp_dbg.c dvmh_omp_event.c\
 	dvmh_omp_thread_info.c dvmh_omp_event_analyzer.c\
 	dvmh_omp_interval.c	 -qopenmp &&\
