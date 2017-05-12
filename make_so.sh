006250c9f0f7e3ad1b6da64256d62be5d0db0c0c
Nurzhan Saktaganov <nur92world@mail.ru>
2017-05-12 20:08:42 +0300

    make_so produces .so file

--- make_so.sh
+++ make_so.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 icc -Wall -shared -qopenmp -fPIC\
 	-Wl,--version-script=exportmap.exp\
-	-o omp_dbg.lib\
+	-o omp_dbg.so\
 	list.c stack.c context_string.c context_descriptor.c\
 	register_context.c omp_dbg.c dvmh_omp_event.c\
 	dvmh_omp_thread_info.c dvmh_omp_event_analyzer.c\
